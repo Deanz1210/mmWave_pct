279c98b15459139c9ef3dd51d2eb471d96022da0
Dean <dean40253@gmail.com>
2023-12-05 18:18:11 +0800

    同時儲存

--- test/v6-class.py
+++ test/v6-class.py
@@ -9,9 +9,11 @@ import time
 
 # 定義一個數據文件類，用於處理與文件相關的操作
 class DataFile:
-    def __init__(self, folder_path):
+    def __init__(self, folder_path, raw_folder_path):
         self.folder_path = folder_path
+        self.raw_folder_path = raw_folder_path
         self.current_file = None
+        self.raw_file = None
 
     # 打開一個新的文件並寫入數據
     def open(self, data):
@@ -33,6 +35,27 @@ class DataFile:
         if self.current_file is not None:
             self.current_file.close()
             self.current_file = None
+
+# 打開一個新的文件並寫入數據
+    def raw_open(self, data):
+        timestamp = time.strftime("%Y%m%d-%H%M%S")
+        filename = f"{timestamp}.json"
+        os.makedirs(self.raw_folder_path, exist_ok=True)
+        file_path = os.path.join(self.raw_folder_path, filename)
+        self.raw_file = open(file_path, 'w')
+        json.dump(data, self.raw_file)
+        self.raw_file.write("\n")
+
+    # 寫入數據到當前文件
+    def raw_write(self, data):
+        json.dump(data, self.raw_file)
+        self.raw_file.write("\n")
+
+    # 關閉當前文件
+    def raw_close(self):
+        if self.raw_file is not None:
+            self.raw_file.close()
+            self.raw_file = None
 
 # 定義停止條件類，用於決定何時停止數據收集
 class StopCondition:
@@ -57,10 +80,10 @@ class TimeStopCondition(StopCondition):
 
 # 定義一個雷達數據收集器類，用於收集雷達數據
 class RadarDataCollector:
-    def __init__(self, port, tiltAngle, height, folder_path, stop_condition):
+    def __init__(self, port, tiltAngle, height, folder_path, raw_folder_path, stop_condition):
         self.port = serial.Serial(port, baudrate = 921600, timeout = 0.5)
         self.radar = pct.Pct(self.port, tiltAngle=tiltAngle, height=height)
-        self.data_file = DataFile(folder_path)
+        self.data_file = DataFile(folder_path, raw_folder_path)
         self.stop_condition = stop_condition
         self.collected_frames = 0
         self.prev_fn = 0
@@ -100,12 +123,12 @@ class RadarDataCollector:
             return
         filtered_frame = RadarDataCollector.filter_data(frame) #過濾設定
         if self.data_file.current_file is None:
-            #self.data_file.open(frame)     #無過濾
+            self.data_file.raw_open(frame)     #無過濾
             self.data_file.open(filtered_frame)
         else:
-            #self.data_file.write(frame)    #無過濾
+            self.data_file.raw_write(frame)    #無過濾
             self.data_file.write(filtered_frame)
 
 # 創建一個雷達數據收集器實例並開始收集數據
-collector = RadarDataCollector("/dev/ttyS0", 45, 2.41, "/home/led/mmwave-project/mmwave/mmWave_pct/test/out", TimeStopCondition(10))
+collector = RadarDataCollector("/dev/ttyS0", 45, 2.41, "/home/led/mmwave-project/mmwave/mmWave_pct/test/out", "/home/led/mmwave-project/mmwave/mmWave_pct/test/out-complete",TimeStopCondition(10))
 collector.collect_data()
